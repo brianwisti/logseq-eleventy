7193e6912ddb35d6a23181e362bb984a637b4bef
Brian Wisti <brianwisti@pobox.com>
2023-04-15 12:00:00 -0700

    some SCSS and rearranging children

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const SassHandler = require("./src/_11ty/handlers/SassHandler.js");
+
 module.exports = function (eleventyConfig) {
+  // Let Eleventy handle SASS
+  //  see https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", SassHandler);
+
   return {
     dir: {
       input: "src",
@@ -6,5 +13,6 @@ module.exports = function (eleventyConfig) {
       data: "_data",
       output: "_site",
     },
+    passthroughFileCopy: true,
   };
 };
