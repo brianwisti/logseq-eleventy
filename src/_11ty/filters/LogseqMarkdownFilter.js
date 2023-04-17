293f77dcee098ee4cc6bafc94800de9173c0824e
Brian Wisti <brianwisti@pobox.com>
2023-04-17 15:16:55 -0700

    video embeds

--- src/_11ty/filters/LogseqMarkdownFilter.js
+++ src/_11ty/filters/LogseqMarkdownFilter.js
@@ -16,6 +16,10 @@ const admonitionBlock = XRegExp(`
   \\n \\#\\+ END_\\k<category> $
 `, 'xm');
 
+const videoBlock = XRegExp(`
+  {{video \\s https://www\\.youtube\\.com/watch\\?v=(?<videoId> .+?)}}
+`, 'x');
+
 const md = LogseqMarkdownHandler();
 
 function getPagePermalink(pageName) {
@@ -30,6 +34,16 @@ function handleAdmonitions(content) {
     const admonitionClass = groups.category.toLowerCase();
     const mdContent = md.render(groups.content);
     return `<aside class="${admonitionClass}"><header>${admonitionClass}</header>${mdContent}</aside>`;
+  });
+}
+
+function handleVideoEmbeds(content) {
+  return XRegExp.replace(content, videoBlock, (match, ...args) => {
+    const groups = args.pop();
+    const videoId = groups.videoId;
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    const embed = `<iframe width="560" height="315" src="${embedUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    return embed;
   });
 }
 
@@ -48,6 +62,7 @@ function handleWikiLinks(content) {
 module.exports = function(content) {
   content = handleWikiLinks(content);
   content = handleAdmonitions(content);
+  content = handleVideoEmbeds(content);
 
   return md.render(content);
 }
