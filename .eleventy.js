376d0a58c962c4c517809c4264ade2d66ad1d2d7
Brian Wisti <brianwisti@pobox.com>
2023-04-17 16:05:40 -0700

    publishable

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 const LogseqMarkdownFilter = require("./src/_11ty/filters/LogseqMarkdownFilter.js");
 const SassHandler = require("./src/_11ty/handlers/SassHandler.js");
 
+const plausibleShortcode = require("./src/_11ty/shortcodes/plausibleShortcode.js");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/*.png");
   // Let Eleventy handle SASS
   //  see https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
   eleventyConfig.addTemplateFormats("scss");
   eleventyConfig.addExtension("scss", SassHandler);
 
   eleventyConfig.addNunjucksFilter("logseq_markdown", LogseqMarkdownFilter);
+
+  eleventyConfig.addNunjucksShortcode("plausible", plausibleShortcode);
 
   return {
     dir: {
---
+++ src/_11ty/shortcodes/plausibleShortcode.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+  let analyticsWanted = process.env.INCLUDE_ANALYTICS;
+  let plausibleDomain = process.env.PLAUSIBLE_DOMAIN;
+
+  if (analyticsWanted) {
+    return `<script async defer data-domain="${plausibleDomain}"
+      src="https://plausible.io/js/plausible.js">
+    </script>`;
+  }
+
+  return "";
+};
